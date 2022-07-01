dcd49635ed7d1e0461d980b8da3e1725d99e8231
Agnes <agsawicka@gmail.com>
2022-07-01 13:47:25 -0400

    commit tree 62951703d14918237c9f5bf5d26cee7c01fdbfe2

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+
+    function clickAlert() {
+    alert('I was clicked!');
+    }
+
+    input.addEventListener('click', clickAlert);
 }
+
+// const element =dozument.getElementByID("button")
+
+// element.addEventListeners('click', function(){
+//     alert("I was clicked")
+// })
+addingEventListener()
